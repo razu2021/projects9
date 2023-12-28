e6ae04859beee81076b861f3a029ffadefbe7598
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2023-12-28 17:55:53 +0600

    construction company projects

---
+++ assets/js/owl.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function(){
+    $(".index_banner_slider").owlCarousel({
+        items:1,
+        autoplay:true,
+        loop:true,
+        nav: true,
+        dots:false,
+        marginbottom:0,
+        navText: ["<span class='prev'>  <  </span>", "<span class='next'> > </span>"],
+        responsive:{
+            0:{
+                items:1,
+                nav: false,
+            },
+            600:{
+                items:1,
+                nav: false,
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+  });
+// index service slider end here 
+$(document).ready(function(){
+    $(".future_image_slider").owlCarousel({
+        items:3,
+        autoplay:true,
+        loop:true,
+        nav: true,
+        dots:false,
+        margin:10,
+        navText: ["<span class='future_prev'>  <  </span>", "<span class='future_next'> > </span>"],
+        responsive:{
+            0:{
+                items:1,
+                nav: false,
+            },
+            600:{
+                items:1,
+                nav: false,
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+  });
+// index service slider end here 
---
+++ assets/js/swiper.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,  // Corrected property name for loop
+    autoplay: {
+      delay: 2000,  // Set your desired autoplay delay in milliseconds
+    },
+  });
